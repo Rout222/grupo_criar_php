e4525d7a861681e8ef3fdd64c913d8dd60362d51
Guilherme <guilherme.cardoso@valecard.com.br>
2020-03-22 20:16:56 -0300

    Adicionado exceção

--- app_php/controladores/VoltasController.php
+++ app_php/controladores/VoltasController.php
@@ -3,7 +3,8 @@
 	namespace controladores;
 	use services\VoltasServices;
 	use services\PilotosServices;
-
+	use Exception;
+	
 	require_once('./services/VoltasServices.php');
 	require_once('./services/PilotosServices.php');
 
@@ -13,9 +14,13 @@
 		
 		public function __construct($arquivo)
 		{
+			if(empty($arquivo))
+				throw new Exception("Arquivo vazio");
+
 			$this->service = new VoltasServices($arquivo);
 
 			$this->pilotoService = $this->service->pilotosService;
+
 		}
 
 		public function processaArquivo(){
